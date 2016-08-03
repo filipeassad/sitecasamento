3ecb30a4f1a3c0455bb1a03c44b00685b8db7edb
Filipe <filipeassad@gmail.com>
2016-08-03 19:25:33 -0400

    vai porra

--- sitecasamento/urls.py
+++ sitecasamento/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url,patterns
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
                        url(r'^admin/', admin.site.urls),
