ffc1b30b085151b9ca4f62b6a71c834702560c35
Filipe <filipeassad@gmail.com>
2016-08-09 20:48:21 -0400

    vai porra

--- sitecasamento/urls.py
+++ sitecasamento/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import include, url,patterns
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-                       url(r'^admin/', admin.site.urls),
-                       url(r'', include('casamento.urls'))) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [ url(r'^admin/', admin.site.urls),
+                url(r'', include('casamento.urls'))] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
