15715973e4307ead1a092668b4921bd68b29b7d0
Filipe <filipeassad@gmail.com>
2016-08-10 09:03:57 -0400

    alteracao dos arquivos urls.py

--- casamento/urls.py
+++ casamento/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from . import views
 
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^$', views.tela_inicial),
     url(r'^local', views.local_casamento),
     url(r'^listapresentes', views.listapresentes),
-    url(r'^presenca', views.presenca),
-]
+    url(r'^presenca', views.presenca)
+)
--- sitecasamento/urls.py
+++ sitecasamento/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [ url(r'^admin/', admin.site.urls),
-                url(r'', include('casamento.urls'))] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = patterns('',
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('casamento.urls'))
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
